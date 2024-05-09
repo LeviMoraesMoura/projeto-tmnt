f7ead0101815a00914ca0d0f0a984f5dbb3fb243
LeviMoraesMoura <167358574+LeviMoraesMoura@users.noreply.github.com>
2024-05-09 15:37:16 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,33 @@
+const botoes = document.querySelectorAll(".botao");
+
+const personagens = document.querySelectorAll(".personagem");
+
+botoes.forEach((botao, indice) => {
+
+    botao.addEventListener("click", () => {
+
+        desselecionsarBotao();
+
+        desselecionarPersonagem();
+
+        botao.classList.add("selecionado");
+
+        personagens[indice].classList.add("selecionado");
+    });
+});
+
+function desselecionarPersonagem() {
+
+    const personagemSelecionado = document.querySelector(".personagem.selecionado");
+
+    personagemSelecionado.classList.remove("selecionado");
+
+}
+
+function desselecionsarBotao() {
+
+    const botaoSelecionado = document.querySelector(".botao.selecionado");
+
+    botaoSelecionado.classList.remove("selecionado");
+    
+}
